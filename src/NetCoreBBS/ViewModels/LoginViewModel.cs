0087f3503cd4330fd0d8d507dabfd694952591bf
meng670830305 <meng670830305@gmail.com>
2022-06-30 16:18:00 +0900

    プロジェクト ファイルを追加します。

---
+++ src/NetCoreBBS/ViewModels/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreBBS.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "アカウント")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "パスワード")]
+        public string Password { get; set; }
+
+        [Display(Name = "パスワードを入力のまま")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "ユーザー名")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(16, ErrorMessage = "{0} は {2}桁以上に入力してください。", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "パスワード")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "パスワード確認")]
+        [Compare("Password", ErrorMessage = "パスワードが一致ではありません。")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
